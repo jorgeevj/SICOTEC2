89a78e7c91e63b876f7253e3751d50a2bb22bee2
jorge villavicencio <jorgeevj@gmail.com>
2015-09-28 10:10:58 -0500

    cargando modificaciones

--- sicotec2-war/src/java/controladora/Cotizacion/CotizacionMB.java
+++ sicotec2-war/src/java/controladora/Cotizacion/CotizacionMB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controladora;
+package controladora.Cotizacion;
 
 import bo.AlmacenBO;
 import bo.CotizacionBO;
--- sicotec2-war/src/java/controladora/inicio/indexMB.java
+++ sicotec2-war/src/java/controladora/inicio/indexMB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controladora;
+package controladora.inicio;
 
 import entidades.Permiso;
 import java.util.ArrayList;
